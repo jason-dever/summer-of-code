cb11e1d19f39e2e5b825d59c0370bebecf646d2b
Jason Dever <jasonello22@gmail.com>
2023-08-25 16:37:34 -0400

    wrote a fill-in-the-blanks test method for all types of moves and make/unmake

--- Chess-Engine/c++/src/test.cpp
+++ Chess-Engine/c++/src/test.cpp
@@ -3,10 +3,13 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <vector>
+#include <algorithm>
 #include "board.hpp"
 
 using std::cout;
 using std::clog;
+using std::vector;
 
 void printBitboard(uint64_t bitboard) {
     int_fast8_t rank = 8;
@@ -31,6 +34,14 @@ void printBitboard(uint64_t bitboard) {
     cout << "\n     Bitset value: " << bitboard << "d\n\n";
 }
 
+enum BoardMethodsEnum { makeMove, unmakeMove };
+std::string results[2] = {" fail", " pass" };
+
+TestResult::TestResult(int test_overhead, bool test_result) {
+        overhead = test_overhead;
+        result = test_result;
+}
+
 void testMakeUnmake() {
     // quietMoves();
     // captures();
@@ -43,73 +54,12 @@ void testMakeUnmake() {
 }
 
 void testQuietMoves() {
-    testMakeQuietMoves();
-    testUnmakeQuietMoves();
+    vector<uint32_t> moves = { 0xf0130458, 0xf020066b, 0xf04005d2, 
+                               0xf06007ad, 0xf0800107, 0xd0a00d3b };
 
-    cout << "\n";
-}
-
-void testCastling() {
-    testMakeCastling();
-    testUnmakeCastling();
-
-    cout << "\n";
-}
-
-void testMakeQuietMoves() {
-    Board board;
-    Board positions[7] { 
+    vector<Board> positions = { 
         Board("r2qkbnr/ppp4p/2npbp2/3Np1p1/2BPP2B/5Q2/PPP2PPP/R3K1NR w KQkq g6 0 8"), 
-
-        Board("r2qkbnr/ppp4p/2npbp2/3Np1p1/2BPP3/5QB1/PPP2PPP/R3K1NR b KQkq - 1 8"),
-        Board("r2qkbnr/ppp4p/2np1p2/3Np1p1/2BPP1b1/5QB1/PPP2PPP/R3K1NR w KQkq - 2 9"),
-        Board("r2qkbnr/ppp4p/2np1p2/3Np1p1/2BPP1b1/Q5B1/PPP2PPP/R3K1NR b KQkq - 3 9"),
-        Board("r2qkbnr/ppp4p/3p1p2/3Np1p1/1nBPP1b1/Q5B1/PPP2PPP/R3K1NR w KQkq -- 4 10"),
-        Board("r2qkbnr/ppp4p/3p1p2/3Np1p1/1nBPP1b1/Q5B1/PPP2PPP/3RK1NR b Kkq - 5 10"),
-        Board("r2q1bnr/pppk3p/3p1p2/3Np1p1/1nBPP1b1/Q5B1/PPP2PPP/3RK1NR w K - 6 11") 
-    };
-
-    uint32_t moves[6] { 0xf0130458, 0xf020066b, 0xf04005d2, 
-                        0xf06007ad, 0xf0800107, 0xd0a00d3b };
-    
-    
-    bool test_failed = false;
-    bool move_failed;
-
-    int overhead = 0;
-
-    for (int i = 0; i <= 5; i++) {
-        board = positions[i]; 
-
-        auto start = std::chrono::high_resolution_clock::now();
-        board.makeMove(moves[i]);
-        auto stop = std::chrono::high_resolution_clock::now();
-
-        overhead += std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start).count();
-
-        move_failed = !(board == positions[i+1]);
-        test_failed |= move_failed;
-
-        if (move_failed) {
-            clog << "Make quiet move " << i << "fail\n";
-
-            clog << "Expected:\n";
-            positions[i+1].printOut();
-
-            clog << "Actual:\n";
-            board.printOut();
-        }
-    }
-
-    std::string results[2] { " pass", " fail" };
-    clog << "Make quiet move" << results[test_failed] << ", Avg overhead: " << overhead/6 << "ns\n";
-}
-
-void testUnmakeQuietMoves() {
-    Board board;
-    Board positions[7] { 
-        Board("r2qkbnr/ppp4p/2npbp2/3Np1p1/2BPP2B/5Q2/PPP2PPP/R3K1NR w KQkq g6 0 8"), 
-
+        
         Board("r2qkbnr/ppp4p/2npbp2/3Np1p1/2BPP3/5QB1/PPP2PPP/R3K1NR b KQkq - 1 8"),
         Board("r2qkbnr/ppp4p/2np1p2/3Np1p1/2BPP1b1/5QB1/PPP2PPP/R3K1NR w KQkq - 2 9"),
         Board("r2qkbnr/ppp4p/2np1p2/3Np1p1/2BPP1b1/Q5B1/PPP2PPP/R3K1NR b KQkq - 3 9"),
@@ -117,39 +67,85 @@ void testUnmakeQuietMoves() {
         Board("r2qkbnr/ppp4p/3p1p2/3Np1p1/1nBPP1b1/Q5B1/PPP2PPP/3RK1NR b Kkq - 5 10"),
         Board("r2q1bnr/pppk3p/3p1p2/3Np1p1/1nBPP1b1/Q5B1/PPP2PPP/3RK1NR w K - 6 11") 
     };
-
-    uint32_t moves[6] { 0xf0130458, 0xf020066b, 0xf04005d2, 
-                        0xf06007ad, 0xf0800107, 0xd0a00d3b };
     
-    bool test_failed = false;
-    bool move_failed;
+    testMakeQuietMoves(moves, positions);
 
+    std::reverse(positions.begin(), positions.end());
+    std::reverse(moves.begin(), moves.end());
+
+    testUnmakeQuietMoves(moves, positions);
+
+    cout << "\n";
+}
+
+void testMakeQuietMoves(vector<uint32_t> moves, vector<Board> positions) {
+    TestResult out = testMoveTemplate(moves, positions, makeMove);
+
+    clog << "Make quiet moves" << results[out.result] << 
+        ". Average overhead: " << out.overhead/moves.size() << "ns\n"; 
+}
+
+void testUnmakeQuietMoves(vector<uint32_t> moves, vector<Board> positions) {
+    TestResult out = testMoveTemplate(moves, positions, unmakeMove);
+
+    clog << "Unmake quiet moves" << results[out.result] << 
+        ". Average overhead: " << out.overhead/moves.size() << "ns\n"; 
+}
+
+TestResult testMoveTemplate(vector<uint32_t> moves, vector<Board> positions, int method_to_perform) {
+    Board board;
+
+    bool test_passed = true;
     int overhead = 0;
 
-    for (int i = 5; i >= 0; i--) {
-        board = positions[i+1];
+    for (uint32_t i = 0; i < moves.size(); i++) {
+        board = positions[i];
 
-        auto start = std::chrono::high_resolution_clock::now();
-        board.unmakeMove(moves[i]);
-        auto stop = std::chrono::high_resolution_clock::now();
-
-        overhead += std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start).count();
-
-        move_failed = !(board == positions[i]);
-        test_failed |= move_failed;
-
-        if (move_failed) {
-            clog << "Unmake quiet move " << i << "fail.\n";
-            
-            clog << "Expected:\n";
-            positions[i].printOut();
-
-            clog << "Actual:\n";
-            board.printOut();
-        } 
+        overhead += performMethodAndGetOverhead(moves[i], &board, method_to_perform);
+        test_passed &= assertBoardIsCorrect(board, positions, i, method_to_perform);
     }
-    std::string results[2] { " pass", " fail" };
-    clog << "Unmake quiet move" << results[test_failed] << ", Avg overhead: " << overhead/6 << "ns\n";
+
+    return TestResult(overhead, test_passed);
+}
+
+int performMethodAndGetOverhead(const uint32_t move, Board* board_ptr, const int method) {
+    std::chrono::_V2::system_clock::time_point start;
+    std::chrono::_V2::system_clock::time_point stop;
+
+    /* By dereferencing the board beforehand, 
+    no additional overhead is incurred during the benchmark.*/
+    Board dereferenced_board = *board_ptr;
+
+    if (method == makeMove) {
+        start = std::chrono::high_resolution_clock::now();
+        dereferenced_board.makeMove(move);
+        stop = std::chrono::high_resolution_clock::now();
+    }
+    else {
+        start = std::chrono::high_resolution_clock::now();
+        dereferenced_board.unmakeMove(move);
+        stop = std::chrono::high_resolution_clock::now();
+    }
+    *board_ptr = dereferenced_board;
+
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start).count();
+}
+
+bool assertBoardIsCorrect(Board board, vector<Board> positions, int index, int method) {    
+    bool move_passed = board == positions[index+1]; 
+
+    if (!move_passed) {
+        std::string methods[2] {"Make ", "Unmake "};
+
+        clog << methods[method] << "move " << index << " fail\n";
+
+        clog << "Expected:\n";
+        positions[index+1].printOut();
+
+        clog << "Actual:\n";
+        board.printOut();
+    }
+    return move_passed;
 }
 
 void testMakeCastling() {
@@ -167,8 +163,6 @@ void testMakeCastling() {
     uint32_t moves[4] = { 0xf0403000, 0xc060f000, 0xf040f000, 0xc0603000 }; 
 
     bool test_failed = false;
-    bool move_failed;
-
     int overhead = 0;
 
     for (int i = 1; i <= 2; i++) {
@@ -180,11 +174,11 @@ void testMakeCastling() {
 
         overhead += std::chrono::duration_cast<std::chrono::nanoseconds>(stop-start).count();
 
-        move_failed |= !(board == positions[i]);
+        bool move_failed = board != positions[i];
         test_failed |= move_failed;
 
         if (move_failed) {
-            clog << "Unmake quiet move " << i << "fail.\n";
+            clog << "Make castling " << i << "fail.\n";
             
             clog << "Expected:\n";
             positions[i].printOut();
@@ -193,8 +187,15 @@ void testMakeCastling() {
             board.printOut();
         } 
     }
+    board = positions[0];
+
+    for (int i = 3; i <= 4; i++) {
+        
+    }
+    
+
     std::string results[2] { " pass", " fail" };
-    clog << "Make castling" << results[test_failed] << ", Avg overhead: " << overhead/6 << "ns\n";
+    clog << "Make castling" << results[test_failed] << ", Avg overhead: " << overhead/4 << "ns\n";
 }
 
 void testUnmakeCastling() {
--- Chess-Engine/c++/src/test.hpp
+++ Chess-Engine/c++/src/test.hpp
@@ -3,20 +3,33 @@
 #include "precompute.hpp"
 #include "board.hpp"
 
-void printBitboard(uint64_t bitboard);
+struct TestResult {
+    int overhead;
+    bool result;
+
+    TestResult(int, bool);
+};
+
+void printBitboard(uint64_t);
 
 void testMakeUnmake();
 
 void testQuietMoves();
-void testMakeQuietMoves();
-void testUnmakeQuietMoves();
+void testMakeQuietMoves(std::vector<uint32_t>, std::vector<Board>);
+void testUnmakeQuietMoves(std::vector<uint32_t>, std::vector<Board>);
 
 void testCaptures();
 
 void testCastling();
+void testMakeCastling();
+void testUnmakeCastling();
 
 void enPassant();
 void pawnPromotions();
 void pawnPushes();
+
+TestResult testMoveTemplate(std::vector<uint32_t>, std::vector<Board>, int);
+int performMethodAndGetOverhead(const uint32_t, Board*, const int);
+bool assertBoardIsCorrect(Board, std::vector<Board>, int, int);
 
 void perft();
