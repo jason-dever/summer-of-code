7d9ea9401e131a3ebb4cfb54077e13f5552bd550
Jason Dever <jasonello22@gmail.com>
2024-01-07 18:28:36 -0500

    added empty declarations for philosophy work strings

--- WhoAmI/src/actual-work.c
+++ WhoAmI/src/actual-work.c
@@ -1,3 +1,10 @@
 // This is where my philosophy work originally was.
 // I figured there was no need for it to be public
 // on GitHub, so I removed it.
+
+#include "actual-work.h"
+
+const char* past_self_info = "";
+const char* academic_self_info = "";
+const char* future_self_info = "";
+const char* true_self_info = "";
