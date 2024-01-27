ea208688c7cfb8c36548eb1816b35abe8f3d70e7
Jason Dever <jasonello22@gmail.com>
2024-01-27 18:32:00 -0500

    optimized solution to not use recursion

--- Coding-Problems/CCC/good-fours-and-fives.cpp
+++ Coding-Problems/CCC/good-fours-and-fives.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 
-void countSolutions(int num, int& num_ways) {
-    if (num == 0)
-        ++num_ways;
-    
-    if (num <= 3)
-        return;
-
-    countSolutions(num-4, num_ways);
-    num_ways += (num%5 == 0);
-}
-
 int main() {
     int num;
     std::cin >> num;
     
-    int num_ways = 0;
-    countSolutions(num, num_ways);
+    auto num_ways = 0;
+
+    while (num > 0) {
+        num_ways += (num%5 == 0);
+        num -= 4;
+    }
+    num_ways += (num == 0);
 
     std::cout << num_ways << '\n';
     return 0;
