b924ef7f85a97e14003f1c326247f7e2ee2aaccb
Jason Dever <jasonello22@gmail.com>
2024-02-17 18:05:12 -0500

    added commandline arguments

--- shorts-generator/shorts-generator.py
+++ shorts-generator/shorts-generator.py
@@ -1,3 +1,4 @@
+import argparse
 from moviepy import *
 import os
 import random
@@ -6,11 +7,11 @@ from TTS.api import TTS
 
 video_end_buffer = 1.25
 
-def generateSpeech(content, speech_path):
+def generateSpeech(text, speech_path):
     device = "cuda"
 
     tts = TTS(model_name="tts_models/en/jenny/jenny", progress_bar=False).to(device)
-    tts.tts_to_file(text=content, file_path=speech_path)
+    tts.tts_to_file(text=text, file_path=speech_path)
 
 def getTrimmedClip(clip, target_duration):
     len_subclip = int(target_duration+video_end_buffer)
@@ -46,10 +47,10 @@ def filesExist(*args):
         if not os.path.exists(path):
             print(f"File does not exist: {path}")
             files_exist = False
-    
+
     return files_exist
 
-def generateShort(content):
+def generateShort(text, name):
     music_path = "resources/music.mp3"
     video_path = "resources/gameplay.mp4"
     if not filesExist(music_path, video_path):
@@ -60,14 +61,43 @@ def generateShort(content):
         os.mkdir("out")
 
     speech_path = "out/speech.mp3"
-    output_path = "out/short.mp4"
+    output_path = f"out/{name}.mp4"
 
-    generateSpeech(content, speech_path)
+    generateSpeech(text, speech_path)
     audio = getSpeechWithMusic(speech_path, music_path)
     short = getShortWithVideo(audio, video_path)
 
     short.write_videofile(output_path)
     os.remove(speech_path)
 
-content = "generated speech goes here"
-generateShort(content)
+def main():
+    parser = argparse.ArgumentParser(description="Arguments that control the behaviour of the program as it generates a short.")
+    parser.add_argument("-t", "--text", help="text to be put into shorts form; only use this if you are not using --file")
+    parser.add_argument("-f", "--file", help="name of the file to read from; only use this if you are not using --text")
+    parser.add_argument("-n", "--name", help="name of the output file; files are written as mp4 by default.")
+    args = parser.parse_args()
+
+    if args.file and args.text:
+        print("Can't read from a file and a string at the same time. Try just one. Aborting.")
+        return
+
+    if not args.file and not args.text:
+        print("No text to create a short from. Aborting.")
+        return
+
+    text = ""
+
+    if args.file:
+        text = open(args.file).read()
+    elif args.text:
+        text = args.text
+
+    if args.name:
+        name = args.name
+    else:
+        name = "short"
+
+    generateShort(text, name)
+
+if __name__ == "__main__":
+    main()
