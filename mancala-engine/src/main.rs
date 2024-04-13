20fdf287ddfbf8007196b01832200a87f5fcbf92
Jason Dever <jasonello22@gmail.com>
2024-04-12 23:08:48 -0400

    moved mancala-engine into a cargo project

--- mancala-engine/src/main.rs
+++ mancala-engine/src/main.rs
@@ -1,14 +1,15 @@
-const NUM_HOLES: i32 = 6;
+const NUM_HOLES: usize = 6;
 
 struct Board {
-    pebbles: [[u16; NUM_HOLES as usize]; 2],
+    pebbles: [[u16; NUM_HOLES]; 2],
     scores: [u16; 2],
     turn: bool,
+    capture_stack: Vec<u16>,
 }
 
 fn print_board(board: &Board) {
     for i in 0..NUM_HOLES {
-        println!("{} {}", board.pebbles[0][i as usize], board.pebbles[1][(NUM_HOLES-i-1) as usize]);
+        println!("{} {}", board.pebbles[0][i], board.pebbles[1][NUM_HOLES-i-1]);
     }
     println!("scores: {}, {}", board.scores[0], board.scores[1]);
     println!("turn: {}", board.turn);
@@ -21,24 +22,19 @@ fn print_board(board: &Board) {
 */
 
 fn make_move(board: &mut Board, mmove: u16) {
-    let mut idx: i32 = ((mmove & 0xe) >> 1).into();
-    board.pebbles[board.turn as usize][idx as usize] = 0;
+    let mut idx: usize = (((mmove >> 1) & 0x7)+1).into();
+    board.pebbles[usize::from(board.turn)][idx] = 0;
 
     let mut num_pebbles = mmove >> 4;
     let mut side_placing_on = board.turn;
     let mut new_turn = !board.turn;
 
     while num_pebbles > 0 {
-        idx += 1;
         num_pebbles -= 1;
 
         if idx == NUM_HOLES { // Placing on someone's store
             if side_placing_on == board.turn {
-                board.scores[board.turn as usize] += 1;
-
-                if num_pebbles == 0 {
-                    new_turn = board.turn;
-                }
+                board.scores[usize::from(board.turn)] += 1;
             }
             else {
                 // Num pebbles was decremented assuming that a pebble was
@@ -47,11 +43,25 @@ fn make_move(board: &mut Board, mmove: u16) {
                 num_pebbles += 1;
             }
 
-            idx = -1; // idx will be incremented to 0 before being used.
+            idx = 0; 
             side_placing_on = !side_placing_on;
         }
         else {
-            board.pebbles[side_placing_on as usize][idx as usize] += 1;
+            board.pebbles[usize::from(side_placing_on)][idx] += 1;
+            idx += 1;
+        }
+    }
+    // Checks for free turn and captures
+    if side_placing_on == board.turn {
+        if idx == NUM_HOLES  {
+            new_turn = board.turn;
+        }
+        else if board.pebbles[usize::from(side_placing_on)][idx] == 0 {
+            let num_pebbles_captured = board.pebbles[usize::from(!side_placing_on)][idx];
+            board.pebbles[usize::from(!side_placing_on)][idx] = 0;
+
+            board.capture_stack.push(num_pebbles_captured);
+            board.scores[usize::from(board.turn)] += num_pebbles_captured
         }
     }
     board.turn = new_turn;
@@ -62,46 +72,40 @@ fn unmake_move(board: &mut Board, mmove: u16) {
 
     let mut num_pebbles = mmove >> 4;
     let mut side_placing_on = board.turn;
-    let mut idx: i32 = ((mmove & 0xe) >> 1).into();
+    let mut idx: usize = (((mmove >> 1) & 0x7)+1).into();
 
     while num_pebbles > 0 {
-        idx += 1;
         num_pebbles -= 1;
 
         if idx == NUM_HOLES { // Placing on someone's store
             if side_placing_on == board.turn {
-                board.scores[board.turn as usize] -= 1;
+                board.scores[usize::from(board.turn)] -= 1;
             }
             else {
                 num_pebbles += 1;
             }
 
-            idx = -1; // idx will be incremented to 0 before being used.
+            idx = 0;
             side_placing_on = !side_placing_on;
         }
         else {
-            board.pebbles[side_placing_on as usize][idx as usize] -= 1;
+            board.pebbles[usize::from(side_placing_on)][idx] -= 1;
+            idx += 1;
         }
     }
 
-    board.pebbles[board.turn as usize][((mmove & 0xe) >> 1) as usize] = mmove >> 4;
+    board.pebbles[usize::from(board.turn)][usize::from((mmove & 0xe) >> 1)] = mmove >> 4;
 }
 
 fn main() {
     let mut board = Board {
-        pebbles: [[4, 4, 4, 4, 4, 10], [4, 4, 4, 4, 4, 4]],
+        pebbles: [[4, 4, 4, 4, 0, 10], [4, 4, 4, 4, 4, 4]],
         scores: [0, 0],
         turn: false,
+        capture_stack: Vec::with_capacity(NUM_HOLES*2),
     };
     print_board(&board);
-    let new_move: u16 = 0xaa;
+    let new_move = 0x40;
     make_move(&mut board, new_move);
-    print_board(&board);
-    let move2: u16 = 0x53;
-    make_move(&mut board, move2);
-    print_board(&board);
-    unmake_move(&mut board, move2);
-    print_board(&board);
-    unmake_move(&mut board, new_move);
     print_board(&board);
 }
