80253ccf42c431891c9a477210102c6f8824450b
Jason Dever <jasonello22@gmail.com>
2024-04-20 20:17:28 -0400

    added alpha beta pruning and avalanche mancala capability

--- mancala-engine/src/main.rs
+++ mancala-engine/src/main.rs
@@ -1,4 +1,6 @@
 const NUM_POCKETS: usize = 6;
+
+#[derive(PartialEq, Debug)]
 enum GameResult {
     Draw,
     P1Win,
@@ -12,7 +14,7 @@ struct Board {
     turn: bool,
 }
 
-fn make_move(board: &mut Board, mut idx: i16) {
+fn make_move(board: &mut Board, mut idx: i16, game_type: char) {
     let mut num_pebbles = board.pebbles[board.turn as usize][idx as usize];
     let mut side_placing_on = board.turn;
 
@@ -38,9 +40,14 @@ fn make_move(board: &mut Board, mut idx: i16) {
             idx = -1;
             side_placing_on = !side_placing_on;
         }
+        if game_type == 'a' && num_pebbles == 0 && idx != -1 && board.pebbles[side_placing_on as usize][idx as usize] > 1 {
+            num_pebbles = board.pebbles[side_placing_on as usize][idx as usize];
+            board.pebbles[side_placing_on as usize][idx as usize] = 0;
+            // println!("update: {idx}");
+        }
     }
     // Checks for captures
-    if side_placing_on == board.turn && board.pebbles[board.turn as usize][idx as usize] == 1 && board.pebbles[!(board.turn) as usize][(5-idx) as usize] > 0 {
+    if game_type == 'c' && side_placing_on == board.turn && board.pebbles[board.turn as usize][idx as usize] == 1 && board.pebbles[!(board.turn) as usize][(5-idx) as usize] > 0 {
         let num_pebbles_captured = board.pebbles[!board.turn as usize][5-idx as usize];
         board.pebbles[!board.turn as usize][5-idx as usize] = 0;
         board.pebbles[board.turn as usize][idx as usize] = 0;
@@ -84,9 +91,14 @@ fn is_gameover(board: &Board) -> bool {
     return false;
 }
 
-fn get_game_result(board: &Board) -> GameResult {
+fn get_game_result(board: &Board, game_type: char) -> GameResult {
     // It's assumed that the game is over when this fn is called.
-    let difference = board.scores[0] + board.pebbles[0].iter().sum::<i16>() - board.scores[1] - board.pebbles[1].iter().sum::<i16>();
+    let difference = if game_type == 'c' {
+        board.scores[0] + board.pebbles[0].iter().sum::<i16>() - board.scores[1] - board.pebbles[1].iter().sum::<i16>()
+    }
+    else { // Avalanche
+        board.scores[0] - board.scores[1]
+    };
 
     if difference > 0 {
         return GameResult::P1Win;
@@ -99,17 +111,17 @@ fn get_game_result(board: &Board) -> GameResult {
     }
 }
 
-fn eval(board: &Board) -> i64 {
-    return (board.scores[0] - board.scores[1]) as i64;
+fn eval(board: &Board) -> f64 {
+    return (board.scores[0] - board.scores[1]) as f64;
 }
 
 struct MiniMaxOutput {
     best_move: usize,
-    eval: i64,
+    eval: f64,
 }
 
 
-fn minimax(board: &mut Board, depth: i32) -> MiniMaxOutput {
+fn minimax(board: &mut Board, mut alpha: f64, mut beta: f64, depth: i32, game_type: char) -> MiniMaxOutput {
     if depth == 0 {
         return MiniMaxOutput {
             best_move: 0,
@@ -118,41 +130,57 @@ fn minimax(board: &mut Board, depth: i32) -> MiniMaxOutput {
     }
 
     if is_gameover(board) {
+        // This ensures we are winning by as much as possible and losing by 
+        // as little as possible.
+        let mut heuristic = board.scores[0] - board.scores[1];
+        if game_type == 'c' {
+            heuristic += board.pebbles[0].iter().sum::<i16>() - board.pebbles[1].iter().sum::<i16>();
+        };
         return MiniMaxOutput {
             best_move: 0,
-            eval: match get_game_result(board) {
-                    GameResult::P1Win => 10000,
-                    GameResult::P2Win => -10000,
-                    GameResult::Draw => 0,
+            eval: match get_game_result(board, game_type) {
+                    GameResult::P1Win => 10000.0 + heuristic as f64,
+                    GameResult::P2Win => -10000.0 + heuristic as f64,
+                    GameResult::Draw => 0.0,
             }
         }
     }
 
     let mut best_idx = 0 ;
-    let mut best_score = if board.turn == true { i64::MAX } else { i64::MIN };
-    // let mut rewritten = false;
+    let mut best_score = if board.turn == true { f64::MAX } else { f64::MIN };
+
+    let original = board.clone();
+
     for i in 0..NUM_POCKETS {
         if board.pebbles[board.turn as usize][i] != 0 {
-            let original = board.clone();
-            make_move(board, i as i16);
+            make_move(board, i as i16, game_type);
+            let move_score = minimax(board, alpha, beta, depth-1, game_type).eval;
 
-            let move_score = minimax(board, depth-1).eval;
             if (move_score > best_score) ^ original.turn {
-                // rewritten = true;
                 best_score = move_score;
                 best_idx = i;
             }
 
-            *board = original;
+            if original.turn == false { // Maximizer
+                if move_score > beta {
+                    break;
+                }
+                alpha = if alpha > move_score { alpha } else { move_score };
+            }
+            else { // Minimizer
+                if move_score < alpha {
+                    break;
+                }
+                beta = if beta < move_score { beta } else { move_score };
+            }
+            *board = original.clone();
         }
     }
-    // println!("{rewritten}");
     return MiniMaxOutput {
         best_move: best_idx,
         eval: best_score,
     };
 }
-
 
 fn perft(board: &mut Board, depth: i32) -> u64 {
     if depth == 0 || is_gameover(board) {
@@ -165,13 +193,14 @@ fn perft(board: &mut Board, depth: i32) -> u64 {
 
         if num_pebbles != 0 {
             let original = board.clone();
-            make_move(board, i as i16);
+            make_move(board, i as i16, 'c');
             num_nodes += perft(board, depth-1);
-            *board = original;
+            *board = original.clone();
         }
     }
     return num_nodes;
 }
+
 #[cfg(test)]
 mod tests {
     use crate::*;
@@ -227,7 +256,7 @@ mod tests {
         let mut board = boards[0].clone();
 
         for i in 0..boards.len()-1 {
-            make_move(&mut board, moves[i]);
+            make_move(&mut board, moves[i], 'c');
             
             if board != boards[i+1] {
                 println!("expected:");
@@ -278,36 +307,51 @@ mod tests {
 fn main() {
     // use std::time::Instant;
 
+    // engine_vs_engine(minimax2, minimax, 100);
     let mut board = Board {
-        pebbles: [[4, 4, 4, 4, 4, 4], [4, 4, 4, 4, 4, 4]],
-        scores: [0, 0],
+        pebbles: [[0, 6, 4, 4, 2, 5], [3, 4, 2, 2, 0, 1]],
+        scores: [0, 1],
         turn: false,
     };
 
-    let test = minimax(&mut board, 4);
-    println!("{}, {}", test.eval, test.best_move);
+    // let test = minimax(&mut board, 4);
+    // println!("{}, {}", test.eval, test.best_move);
+    let game_type = 'c';
 
     print_board(&board);
     while !is_gameover(&board) {
-        let mut turn = board.turn;
 
-        while turn == board.turn {
+        let mut turn = board.turn;
+        while turn == board.turn && !is_gameover(&board) {
+            let best_move = minimax(&mut board, f64::MIN, f64::MAX, 16, game_type).best_move;
+            println!("move: {best_move}, val: {}", board.pebbles[board.turn as usize][best_move]);
+            make_move(&mut board, best_move as i16, game_type);
+            print_board(&board);
+        }
+        turn = board.turn;
+        while turn == board.turn && !is_gameover(&board) {
             let mut user_str = String::new();
             println!("your turn");
             std::io::stdin().read_line(&mut user_str).unwrap();
             let idx = user_str.trim().parse::<i16>().unwrap();
-            make_move(&mut board, idx);
+            make_move(&mut board, idx, game_type);
             print_board(&board);
         }
 
-        turn = board.turn;
-        while turn == board.turn {
-            let best_move = minimax(&mut board, 12).best_move;
-            println!("move: {best_move}, val: {}", board.pebbles[board.turn as usize][best_move]);
-            make_move(&mut board, best_move as i16);
-            print_board(&board);
+    }
+    if game_type == 'c' {
+        for i in 0..=1 {
+            let mut sum = 0;
+
+            for j in 0..board.pebbles[i].len() {
+                let count = board.pebbles[i][j];
+                sum += count;
+                board.pebbles[i][j] = 0;
+            }
+            board.scores[i] += sum;
         }
     }
+    print_board(&board);
     // for depth in 1..15 {
     //     let timer = Instant::now();
     //     let num_nodes = perft(&mut board, depth);
